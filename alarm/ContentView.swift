0fc3c2460950d757a59daf0ce497778d295e1971
becivells <becivells@qq.com>
2026-05-27 21:12:31 +0800

    Initial Commit

---
+++ alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  alarm
+//
+//  Created by user on 2026/5/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ alarm/alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  alarmApp.swift
+//  alarm
+//
+//  Created by user on 2026/5/27.
+//
+
+import SwiftUI
+
+@main
+struct alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
